bd533c8d692ee00f267a06430ec1ba4e52fa6889
Ravi Shankar <kumarravi199818@gmail.com>
2024-02-02 20:31:25 +0530

    network connection

---
+++ check-network-connection/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import AppConnected from "./check network/App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AppConnected />
+  </React.StrictMode>
+);
